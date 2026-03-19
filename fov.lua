cabcbea7b87a6885693f5ef3ae757850be341d6f
fryingpan0o0 <pansito177@gmail.com>
2026-03-18 21:40:42 -0300

    Update fov.lua

--- fov.lua
+++ fov.lua
@@ -1,7 +1,7 @@
 local Camera = workspace.CurrentCamera
 
 local fovLoop = game:GetService("RunService").RenderStepped:Connect(function()
-    Camera.FieldOfView = 40
+    Camera.FieldOfView = 120
 end)
 
 -- y luego, si alguna vez quisieras detener el bucle, podrías hacer lo siguiente
